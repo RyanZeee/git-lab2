8c420ed14dde8e4a4bb5281cd10f95ed3d15b74c
RyanZeee <rz484521@ohio.edu>
2021-09-01 17:23:59 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+*       File: git-lab-program.cc
+*       Author: Ryan Ziemianski
+*       Date: Sept 1 2021
+*       Description: Print Something
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
